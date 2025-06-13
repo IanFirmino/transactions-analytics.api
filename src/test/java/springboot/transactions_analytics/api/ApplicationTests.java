6b25c73f276c8af48c9ceab99846229cf2c72102
ian firmino <ian.firmino@hotmail.com>
2025-06-12 21:11:42 -0300

    init: springboot initial

---
+++ src/test/java/springboot/transactions_analytics/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.transactions_analytics.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
